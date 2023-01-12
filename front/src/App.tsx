f55b3cefe7ecc2804c3e422067d6ff138ac151f8
NakagamiYuta <guguru0014@gmail.com>
2023-01-13 07:46:56 +0900

    [add](front) ゲームタイマーを追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -18,10 +18,15 @@ export const GameStatContext = createContext({} as SContextType<Boolean>);
 const App = () => {
     const [wasmOk, setWasmOk] = useState<Boolean>(false);
     const [arms, setArms] = useState<RobotArm[]>([]);
+
     const [gameStat, setGameStat] = useState<Boolean>(false);
+    const [gameTime, setGameTime] = useState(0);
+    const [gameTimer, setGameTimer] = useState<NodeJS.Timer>();
 
     const gameFinCallback = () => {
+        alert("!!CONGRATULATIONS!!\nYou're time " + gameTime + "s!");
         setGameStat(false);
+        clearInterval(gameTimer);
     }
 
     useEffect(() => {
@@ -30,6 +35,17 @@ const App = () => {
             setArms([gen_arm(15.0, 45.0)]);
         });
     }, []);
+
+    useEffect(() => {
+        if (gameStat) {
+            setGameTimer(setInterval(() => {
+                setGameTime((gameTime) => gameTime + 1);
+            }, 1000))
+        } else {
+            clearInterval(gameTimer);
+        }
+        setGameTime(0);
+    }, [gameStat]);
 
     return (
         <div
@@ -51,7 +67,7 @@ const App = () => {
                     <b>Erap</b>（Easy Robot Arm Programming）<br/>
                     実世界指向システム 課題 / 中神悠太
                 </p>
-                <p>{ "" + gameStat }</p>
+                <h2>{ "TIME: " + gameTime + "s"}</h2>
             </div>
             <WasmStatContext.Provider value={ wasmOk }>
                 <RobotArmsContext.Provider value={[ arms, setArms ]}>
