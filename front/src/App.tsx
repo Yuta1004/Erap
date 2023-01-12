9fc435f4b0e155b6fb3b35b978fbcaf175a2614a
NakagamiYuta <guguru0014@gmail.com>
2023-01-13 02:44:11 +0900

    [add](front/viewer) とりあえず雑にViewerを作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 
-import init, {  } from "erap_core";
+import init, { gen_arm } from "erap_core";
 import ArmViewer from "./components/ArmViewer";
 
 const App = () => {
+    const [wasmOk, setWasmOk] = useState(false);
+    const [arms, setArms] = useState<{}[]>([]);
+
     useEffect(() => {
-        init().then(() => {});
+        init().then(() => {
+            setWasmOk(true);
+            setArms([
+                gen_arm(20.0,  25.0),
+                gen_arm(18.0,  40.0),
+                gen_arm(35.0, -30.0)
+            ]);
+        });
     }, []);
 
     return (
@@ -18,7 +28,12 @@ const App = () => {
                 display: "flex"
             }}
         >
-            <ArmViewer/>
+            <ArmViewer
+                wasmOk={ wasmOk }
+                x0={ 0 }
+                y0={ 0 }
+                arms={ arms }
+            />
             <Box
                 sx={{
                     boxSizing: "border-box",
--- front/src/components/ArmViewer.tsx
+++ front/src/components/ArmViewer.tsx
@@ -1,29 +1,52 @@
 import { useEffect, useState } from "react";
 
-const ArmViewer = () => {
+import { calc_endpoints } from "erap_core";
+
+interface ArmViewerProps {
+    wasmOk: Boolean,
+    x0: number,
+    y0: number,
+    arms: {}
+}
+
+interface ArmEndpoint {
+    x: number,
+    y: number,
+    theta: number
+}
+
+const ArmViewer = (props: ArmViewerProps) => {
     const [centerX, setCenterX] = useState(0.0);
     const [centerY, setCenterY] = useState(0.0);
     const [context, setContext] = useState<CanvasRenderingContext2D|null>(null);
 
     const cpos = (x: number, y: number): [number, number] => {
-        return [centerX + x, centerY + y];
+        return [centerX + x, centerY - y];
     };
 
     const drawBackGround = () => {
         // X軸
-        context?.beginPath();
-        context?.moveTo(...cpos(-centerX, 0));
-        context?.lineTo(...cpos(centerX, 0));
-        context?.closePath();
-        context?.stroke();
+        context!!.beginPath();
+        context!!.moveTo(...cpos(-centerX, 0));
+        context!!.lineTo(...cpos(centerX, 0));
+        context!!.closePath();
+        context!!.stroke();
 
         // Y軸
-        context?.beginPath();
-        context?.moveTo(...cpos(0, -centerY));
-        context?.lineTo(...cpos(0, centerY));
-        context?.closePath();
-        context?.stroke();
+        context!!.beginPath();
+        context!!.moveTo(...cpos(0, -centerY));
+        context!!.lineTo(...cpos(0, centerY));
+        context!!.closePath();
+        context!!.stroke();
     };
+
+    const drawArms = () => {
+        calc_endpoints(0.0, 0.0, props.arms).forEach((endpoint: ArmEndpoint) => {
+            context!!.beginPath();
+            context!!.arc(...cpos(endpoint.x*5, endpoint.y*5), 15, 0, Math.PI * 2, true);
+            context!!.stroke();
+        });
+    }
 
     useEffect(() => {
         const parentDiv = document.getElementById("viewer");
@@ -40,8 +63,11 @@ const ArmViewer = () => {
     }, []);
 
     useEffect(() => {
-        drawBackGround();
-    }, [context]);
+        if (props.wasmOk) {
+            drawBackGround();
+            drawArms();
+        }
+    }, [props.wasmOk, props.x0, props.y0, props.arms]);
 
     return (
         <div
